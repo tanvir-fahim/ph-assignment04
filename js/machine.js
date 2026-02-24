c01022a6e00c7c20ae40ff8167761e8cb5304d6a
Tanvir-Fahim <mt009.fahim@gmail.com>
2026-02-24 18:49:48 +0600

    Count Job number Successfull.

--- js/machine.js
+++ js/machine.js
@@ -18,3 +18,43 @@ function setTabVisible(sectionId, tabButtonnId)
     const selectTabButton = "px-6 py-2 rounded-md text-base bg-blue-600 text-white shadow-sm";
     document.getElementById(tabButtonnId).className = selectTabButton;
 }
+
+
+function updateDashboardCounts()
+{
+    const allJobNumber = document.getElementById("card-list").children.length;
+    const interviewJobNumber = document.getElementById("interview-list").children.length;
+    const rejectedJobNumber = document.getElementById("rejected-list").children.length;
+
+    document.getElementById("total-number").innerText = allJobNumber;
+    document.getElementById("interview-number").innerText = interviewJobNumber;
+    document.getElementById("rejected-number").innerText = rejectedJobNumber;
+
+    document.getElementById("count-all").innerText = allJobNumber + " jobs";
+    document.getElementById("count-interview").innerText = interviewJobNumber +" of " + allJobNumber + " jobs";
+    document.getElementById("count-rejected").innerText = rejectedJobNumber +" of " + allJobNumber +  " jobs";
+
+
+
+    if(interviewJobNumber === 0)
+    {
+        document.getElementById("empty-interview").classList.remove("hidden");
+        document.getElementById("empty-interview").classList.add("block");
+    }
+    else
+    {
+        document.getElementById("empty-interview").classList.add("hidden");
+        document.getElementById("empty-interview").classList.remove("block");
+    }
+
+    if (rejectedJobNumber === 0)
+    {
+        document.getElementById("empty-rejected").classList.remove("hidden");
+        document.getElementById("empty-rejected").classList.add("block");
+    }
+    else
+    {
+        document.getElementById("empty-rejected").classList.add("hidden");
+        document.getElementById("empty-rejected").classList.remove("block");
+    }
+}
